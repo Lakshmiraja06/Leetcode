d06e80514020dc135f31deaa19fa536b156aa554
LAKSHMI R <lakshmipavithra2006@gmail.com>
2026-02-09 12:08:50 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (24.25%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
@@ -10,18 +10,37 @@
  */
 class Solution {
     public ListNode swapPairs(ListNode head) {
+        // if(head==null || head.next==null){
+        //     return head;
+        // }
+        // ListNode temp=head;
+        // while(temp!=null && temp.next!=null){
+        //     int swap=temp.val;
+        //     temp.val=temp.next.val;
+        //     temp.next.val=swap;
+        //     temp=temp.next.next;
+        // }
+        // //temp=temp.next.next;
+        // return head;
+
         if(head==null || head.next==null){
             return head;
         }
+        ListNode prev = null;
+        ListNode newhead=head.next;
         ListNode temp=head;
         while(temp!=null && temp.next!=null){
-            int swap=temp.val;
-            temp.val=temp.next.val;
-            temp.next.val=swap;
-            temp=temp.next.next;
+            ListNode temp1=temp.next;
+            ListNode nextnode = temp1.next;
+            temp1.next=temp;
+            temp.next=nextnode;
+            if(prev!=null){
+                prev.next=temp1;
+            }
+            prev=temp;
+            temp=nextnode;
         }
-        //temp=temp.next.next;
-        return head;
+        return newhead;
     }
    
 }
