cdc1f1f12647ca4aca075510edf1763a83a62f93
LAKSHMI R <lakshmipavithra2006@gmail.com>
2026-04-17 15:26:56 +0530

    Time: 1 ms (70.08%) | Memory: 43.4 MB (78.85%) - LeetSync

--- 4275-traffic-signal-color/traffic-signal-color.java
+++ 4275-traffic-signal-color/traffic-signal-color.java
@@ -1,15 +1,18 @@
 class Solution {
     public String trafficSignal(int timer) {
-        if(timer==0){
-            return "Green";
-        }else if(timer==30)
-        {
-            return "Orange";
-        }else if(30 < timer && timer<=90){
-            return "Red";
-        }//else  return "Invalid";
+
+        return (timer==0)?"Green":(timer==30)?"Orange":(30<timer && timer<=90)?"Red":"Invalid";
+
+        // if(timer==0){
+        //     return "Green";
+        // }else if(timer==30)
+        // {
+        //     return "Orange";
+        // }else if(30 < timer && timer<=90){
+        //     return "Red";
+        // }//else  return "Invalid";
         
 
-        return "Invalid";    
+        // return "Invalid";    
     }
 }
