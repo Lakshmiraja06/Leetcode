6213bb8808874d4cf7cb7629f6d11f0178a5f0eb
LAKSHMI R <lakshmipavithra2006@gmail.com>
2026-05-20 14:13:12 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (26.49%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -3,8 +3,8 @@ class Solution {
         int find = -1;
         for(int i=0;i<nums.length;i++){
             if(nums[i] == target){
-                if(find == -1) find = i;
-                return find;
+                //if(find == -1) find = i;
+                return i;
             }
         }
         return find;
