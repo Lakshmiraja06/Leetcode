f39c162929662c19e9e690b283cb0b1727d9570d
LAKSHMI R <lakshmipavithra2006@gmail.com>
2026-05-22 19:32:52 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (95.09%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -3,7 +3,6 @@ class Solution {
         int find = -1;
         for(int i=0;i<nums.length;i++){
             if(nums[i] == target){
-                //if(find == -1) find = i;
                 return i;
             }
         }
