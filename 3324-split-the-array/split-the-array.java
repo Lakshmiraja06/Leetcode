f900a54795d58f05c7872d5178d7a55bc29c6f62
LAKSHMI R <lakshmipavithra2006@gmail.com>
2026-06-15 21:49:51 +0530

    Time: 5 ms (7.84%) | Memory: 44.9 MB (6.37%) - LeetSync

--- 3324-split-the-array/split-the-array.java
+++ 3324-split-the-array/split-the-array.java
@@ -14,18 +14,21 @@ class Solution {
                 arr2[l++] = nums[i];
             }
         }
+        int len = arr1.length;
         boolean sub = true;
-        for(int i=0;i<arr1.length;i++){
-            for(int j=i+1;j<arr1.length;j++){
-                if(arr1[i]==arr1[j]) sub = false;
-            }
-        }
         boolean su = true;
-        for(int i=0;i<arr2.length;i++){
-            for(int j=i+1;j<arr2.length;j++){
+        for(int i=0;i<len;i++){
+            for(int j=i+1;j<len;j++){
+                if(arr1[i]==arr1[j]) sub = false;
                 if(arr2[i]==arr2[j]) su = false;
             }
         }
+        // boolean su = true;
+        // for(int i=0;i<arr2.length;i++){
+        //     for(int j=i+1;j<arr2.length;j++){
+        //         if(arr2[i]==arr2[j]) su = false;
+        //     }
+        // }
         return sub&su?true:false;
     }
 }
