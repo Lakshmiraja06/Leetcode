1feca54df8b36c23d2409afd24532f82e4a450ca
LAKSHMI R <lakshmipavithra2006@gmail.com>
2026-06-22 10:23:23 +0530

    Time: 858 ms (9.27%) | Memory: 51.4 MB (38.56%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.java
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.java
@@ -5,13 +5,8 @@ class Solution {
             int pro=1;
             for(int j=i;j<nums.length;j++){
                 if(pro<k){
-                    if(i==j){
-                    pro*=nums[i];
-                    if(pro<k) count++;
-                }else{
                     pro *= nums[j];
                     if(pro<k) count++;
-                }
                 }else{
                     break;
                 }
