95f925ba54acadf20e50f0c174299406b56864c3
LAKSHMI R <lakshmipavithra2006@gmail.com>
2026-06-24 11:03:34 +0530

    Time: 3 ms (57.00%) | Memory: 137.5 MB (73.00%) - LeetSync

---
+++ 3738-make-array-non-decreasing/make-array-non-decreasing.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maximumPossibleSize(int[] nums) {
+        int count=1;
+        int prev=nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(prev<=nums[i]){
+                prev=nums[i];
+                count++;
+            }
+            else continue;
+        }
+        return count;
+    }
+}
