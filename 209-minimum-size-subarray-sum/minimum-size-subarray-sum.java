a22f66a9f29ae3cd3fd7c397adb68e61b5764da5
LAKSHMI R <lakshmipavithra2006@gmail.com>
2026-06-24 06:50:35 +0530

    Time: 2 ms (15.35%) | Memory: 69.1 MB (5.44%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -3,22 +3,42 @@ class Solution {
         int min = Integer.MAX_VALUE;
         int n = nums.length;
         int l=0;
-        int r=0;
+        //int r=0;
         int len=0;
         int sum=0;
-        while(r<n){ 
-            if(sum<target){
-                sum += nums[r];
-                len++;
-                r++;
-            }
-             while(sum>=target){
-                min = Math.min(min, len);
-                sum -= nums[l];
+
+        for(int r=0;r<n;r++){
+
+            sum+=nums[r];
+            len++;
+
+            while(sum>=target){
+                min=Math.min(len,min);
+                sum-=nums[l];
                 len--;
-                l++;  
+                l++;
             }
         }
         return min!=Integer.MAX_VALUE?min:0;
+
+
+
+
+
+
+        // while(r<n){ 
+        //     if(sum<target){
+        //         sum += nums[r];
+        //         len++;
+        //         r++;
+        //     }
+        //      while(sum>=target){
+        //         min = Math.min(min, len);
+        //         sum -= nums[l];
+        //         len--;
+        //         l++;  
+        //     }
+        // }
+        // return min!=Integer.MAX_VALUE?min:0;
     }
 }
