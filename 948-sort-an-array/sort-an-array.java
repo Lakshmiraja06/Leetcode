a82fdeb7e95af1f2483e29600ce9f26f8a4ca85c
LAKSHMI R <lakshmipavithra2006@gmail.com>
2026-07-04 09:53:52 +0530

    Time: 41 ms (19.63%) | Memory: 84 MB (46.10%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -11,5 +11,18 @@ class Solution {
             arr[i] = list.get(i);
         }
         return arr;
+
+        // int i=0;
+        // int i=nums.length;
+        // for(int i=0;i<nums.length;i++){
+        //     for(int j=i+1;j<nums.length;j++){
+        //         if(nums[i]>nums[j]){
+        //             int temp = nums[i];
+        //             nums[i]=nums[j];
+        //             nums[j]=temp;
+        //         }
+        //     }
+        // }
+        //  return nums;
     }
 }
